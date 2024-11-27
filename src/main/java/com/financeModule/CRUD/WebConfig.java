498ef2bd8c73e27a1a27c0115fb617520b3d82a2
Cristobal <cristobal230502@gmail.com>
2024-11-27 16:50:26 -0300

    agregado de configuracion web para aceptar request del front

---
+++ src/main/java/com/financeModule/CRUD/WebConfig.java
@@ -0,0 +1,18 @@
+package com.financeModule.CRUD;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all routes
+                .allowedOrigins("https://tribu-c-2024-2c.onrender.com/") // Replace with your front-end domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow the methods you need
+                .allowedHeaders("Content-Type", "Authorization") // Allow headers if needed
+                .allowCredentials(true); // Allow cookies or credentials if needed
+    }
+}
+
